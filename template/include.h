1c0cac16c5ad34335d179b6534e4d47e6cbca55f
BacklitCat <BacklitCat@gmail.com>
2023-08-16 01:15:28 +0800

    template: include

---
+++ template/include.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream> // stringstream
+#include <algorithm> //sort
+#include <string> //stoi,memset
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdlib> // rand
+#include <queue> // queue,priority_queue
+
+using namespace std;
+
