8cba74aeb8936c5093c4a160f7056017c8d10cb1
BacklitCat <BacklitCat@gmail.com>
2023-08-15 22:48:00 +0800

    leetcode: 5

--- leetcode/5-longest-palindromic-substring.cpp
+++ leetcode/5-longest-palindromic-substring.cpp
@@ -58,11 +58,3 @@ int main() {
     }
     return 0;
 }
-
-pair<int, int> expandAroundCenter(const string& s, int left, int right) {
-    while (left >= 0 && right < s.size() && s[left] == s[right]) {
-        --left;
-        ++right;
-    }
-    return {left + 1, right - 1};
-}
