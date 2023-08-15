1164d4109630d544d329307cb621e93f39f38266
BacklitCat <BacklitCat@gmail.com>
2023-08-15 20:44:34 +0800

    leetcode: 100

---
+++ leetcode/100-same-tree.cpp
@@ -0,0 +1,29 @@
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if ((p == nullptr && q != nullptr) || (p != nullptr && q == nullptr)) {
+            return false;
+        }
+        if (p != nullptr && q != nullptr && p->val != q->val) {
+            return false;
+        }
+        //  p->val == q->val，继续判断子节点
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
