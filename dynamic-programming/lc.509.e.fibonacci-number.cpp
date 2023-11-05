990b453cf431d229b4887a00405018f245a2ba2b
BacklitCat <34826904+BacklitCat@users.noreply.github.com>
2023-11-05 15:03:29 +0800

    lc.509.e.fibonacci-number

--- dynamic-programming/lc.509.e.fibonacci-number.cpp
+++ dynamic-programming/lc.509.e.fibonacci-number.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
-
 // https://leetcode.cn/problems/fibonacci-number/
 
-int ans[30]={0};  // 0 <= n <= 30
+int ans[31]={0};  // 0 <= n <= 30
 
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) { ans[1]=1; return 1;}
+        if(n<=0) return 0;
+        if(n==1) return 1;
+        ans[1]=1;
+        if (ans[n]!=0) return ans[n];
+        for (int i=2; i<=n; i++){
+            if (ans[i]!=0) continue;
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
     }
 };
